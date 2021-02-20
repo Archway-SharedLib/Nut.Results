2175577e6769eb0b5e261ebc22523ed4dafd7e66
kmaru <k-maru@archway.co.jp>
2021-02-21 00:42:09 +0900

    add match methods.

---
+++ src/Nut.Results/Extensions/Match.cs
@@ -0,0 +1,158 @@
+﻿// ReSharper disable CheckNamespace
+
+using System;
+using System.Threading.Tasks;
+
+namespace Nut.Results
+{
+    public static partial class ResultExtensions
+    {
+        // result to result
+        
+        public static Result Match(this in Result source, Func<Result> ok, Func<IError, Result> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            return source.IsOk ? ok() : err(source.errorValue);
+        }
+
+        public static Task<Result> Match(this in Result source, Func<Task<Result>> ok, Func<IError, Result> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            return source.IsOk ? ok() : Task.FromResult(err(source.errorValue));
+        }
+
+        public static Task<Result> Match(this in Result source, Func<Result> ok, Func<IError, Task<Result>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            return source.IsOk ? Task.FromResult(ok()) : err(source.errorValue);
+        }
+
+        public static Task<Result> Match(this in Result source, Func<Task<Result>> ok, Func<IError, Task<Result>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            return source.IsOk ? ok() : err(source.errorValue);
+        }
+
+        public static async Task<Result> Match(this Task<Result> source, Func<Result> ok, Func<IError, Result> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? ok() : err(result.errorValue);
+        }
+
+        public static async Task<Result> Match(this Task<Result> source, Func<Task<Result>> ok, Func<IError, Result> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? await ok() : err(result.errorValue);
+        }
+
+        public static async Task<Result> Match(this Task<Result> source, Func<Result> ok, Func<IError, Task<Result>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? ok() : await err(result.errorValue);
+        }
+
+        public static async Task<Result> Match(this Task<Result> source, Func<Task<Result>> ok, Func<IError, Task<Result>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? await ok() : await err(result.errorValue);
+        }
+        
+        // result to result<T>
+
+        public static Result<T> Match<T>(this in Result source, Func<Result<T>> ok, Func<IError, Result<T>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+        
+            return source.IsOk ? ok() : err(source.errorValue);
+        }
+
+        public static Task<Result<T>> Match<T>(this in Result source, Func<Task<Result<T>>> ok, Func<IError, Result<T>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            return source.IsOk ? ok() : Task.FromResult(err(source.errorValue));
+        }
+
+        public static Task<Result<T>> Match<T>(this in Result source, Func<Result<T>> ok, Func<IError, Task<Result<T>>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            return source.IsOk ? Task.FromResult(ok()) : err(source.errorValue);
+        }
+
+        public static Task<Result<T>> Match<T>(this in Result source, Func<Task<Result<T>>> ok, Func<IError, Task<Result<T>>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            return source.IsOk ? ok() : err(source.errorValue);
+        }
+
+        public static async Task<Result<T>> Match<T>(this Task<Result> source, Func<Result<T>> ok, Func<IError, Result<T>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? ok() : err(result.errorValue);
+        }
+
+        public static async Task<Result<T>> Match<T>(this Task<Result> source, Func<Task<Result<T>>> ok, Func<IError, Result<T>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? await ok() : err(result.errorValue);
+        }
+
+        public static async Task<Result<T>> Match<T>(this Task<Result> source, Func<Result<T>> ok, Func<IError, Task<Result<T>>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? ok() : await err(result.errorValue);
+        }
+
+        public static async Task<Result<T>> Match<T>(this Task<Result> source, Func<Task<Result<T>>> ok, Func<IError, Task<Result<T>>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? await ok() : await err(result.errorValue);
+        }
+    }
+}
---
+++ src/Nut.Results/Extensions/T_Match.cs
@@ -0,0 +1,158 @@
+﻿// ReSharper disable CheckNamespace
+
+using System;
+using System.Threading.Tasks;
+
+namespace Nut.Results
+{
+    public static partial class ResultExtensions
+    {
+        // reulst<T> to result<T>
+
+        public static Result<TResult> Match<T, TResult>(this in Result<T> source, Func<T, Result<TResult>> ok, Func<IError, Result<TResult>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+        
+            return source.IsOk ? ok(source.value) : err(source.errorValue);
+        }
+
+        public static Task<Result<TResult>> Match<T, TResult>(this in Result<T> source, Func<T, Task<Result<TResult>>> ok, Func<IError, Result<TResult>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            return source.IsOk ? ok(source.value) : Task.FromResult(err(source.errorValue));
+        }
+
+        public static Task<Result<TResult>> Match<T, TResult>(this in Result<T> source, Func<T, Result<TResult>> ok, Func<IError, Task<Result<TResult>>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            return source.IsOk ? Task.FromResult(ok(source.value)) : err(source.errorValue);
+        }
+
+        public static Task<Result<TResult>> Match<T, TResult>(this in Result<T> source, Func<T, Task<Result<TResult>>> ok, Func<IError, Task<Result<TResult>>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            return source.IsOk ? ok(source.value) : err(source.errorValue);
+        }
+
+        public static async Task<Result<TResult>> Match<T, TResult>(this Task<Result<T>> source, Func<T, Result<TResult>> ok, Func<IError, Result<TResult>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? ok(result.value) : err(result.errorValue);
+        }
+
+        public static async Task<Result<TResult>> Match<T, TResult>(this Task<Result<T>> source, Func<T, Task<Result<TResult>>> ok, Func<IError, Result<TResult>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? await ok(result.value) : err(result.errorValue);
+        }
+
+        public static async Task<Result<TResult>> Match<T, TResult>(this Task<Result<T>> source, Func<T, Result<TResult>> ok, Func<IError, Task<Result<TResult>>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? ok(result.value) : await err(result.errorValue);
+        }
+
+        public static async Task<Result<TResult>> Match<T, TResult>(this Task<Result<T>> source, Func<T, Task<Result<TResult>>> ok, Func<IError, Task<Result<TResult>>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? await ok(result.value) : await err(result.errorValue);
+        }
+
+        // reulst<T> to result
+
+        public static Result Match<T>(this in Result<T> source, Func<T, Result> ok, Func<IError, Result> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            return source.IsOk ? ok(source.value) : err(source.errorValue);
+        }
+
+        public static Task<Result> Match<T>(this in Result<T> source, Func<T, Task<Result>> ok, Func<IError, Result> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            return source.IsOk ? ok(source.value) : Task.FromResult(err(source.errorValue));
+        }
+
+        public static Task<Result> Match<T>(this in Result<T> source, Func<T, Result> ok, Func<IError, Task<Result>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            return source.IsOk ? Task.FromResult(ok(source.value)) : err(source.errorValue);
+        }
+
+        public static Task<Result> Match<T>(this in Result<T> source, Func<T, Task<Result>> ok, Func<IError, Task<Result>> err)
+        {
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+            
+            return source.IsOk ? ok(source.value) : err(source.errorValue);
+        }
+
+        public static async Task<Result> Match<T>(this Task<Result<T>> source, Func<T, Result> ok, Func<IError, Result> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? ok(result.value) : err(result.errorValue);
+        }
+
+        public static async Task<Result> Match<T>(this Task<Result<T>> source, Func<T, Task<Result>> ok, Func<IError, Result> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? await ok(result.value) : err(result.errorValue);
+        }
+
+        public static async Task<Result> Match<T>(this Task<Result<T>> source, Func<T, Result> ok, Func<IError, Task<Result>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? ok(result.value) : await err(result.errorValue);
+        }
+
+        public static async Task<Result> Match<T>(this Task<Result<T>> source, Func<T, Task<Result>> ok, Func<IError, Task<Result>> err)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (ok is null) throw new ArgumentNullException(nameof(ok));
+            if (err is null) throw new ArgumentNullException(nameof(err));
+
+            var result = await source.ConfigureAwait(false);
+            return result.IsOk ? await ok(result.value) : await err(result.errorValue);
+        }
+    }
+}
