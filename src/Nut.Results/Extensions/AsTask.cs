54922421c6df44060c25a82900b75a6f92a1caf1
kmaru <k-maru@archway.co.jp>
2021-06-12 09:15:41 +0900

    add astask doc

--- src/Nut.Results/Extensions/AsTask.cs
+++ src/Nut.Results/Extensions/AsTask.cs
@@ -6,6 +6,11 @@ namespace Nut.Results
 {
     public static partial class ResultExtensions
     {
+        /// <summary>
+        /// <see cref="Task{T}"/> として型指定された入力を返します。
+        /// </summary>
+        /// <param name="source"><see cref="Task{T}"/>として型指定する<see cref="Result"/></param>
+        /// <returns><see cref="Task{T}"/> として型指定された入力</returns>
         public static Task<Result> AsTask(this Result source) => Task.FromResult(source);
     }
 }
--- src/Nut.Results/Extensions/T_AsTask.cs
+++ src/Nut.Results/Extensions/T_AsTask.cs
@@ -6,6 +6,12 @@ namespace Nut.Results
 {
     public static partial class ResultExtensions
     {
+        /// <summary>
+        /// <see cref="Result{T}"/>をもった<see cref="Task{T}"/>として型指定された入力を返します。
+        /// </summary>
+        /// <typeparam name="T"><see cref="Result{T}"/>の値の型</typeparam>
+        /// <param name="source"><see cref="Result{T}"/>をもった<see cref="Task{T}"/>として型指定する<see cref="Result{T}"/></param>
+        /// <returns><see cref="Result{T}"/>をもった<see cref="Task{T}"/>として型指定された入力</returns>
         public static Task<Result<T>> AsTask<T>(this Result<T> source) => Task.FromResult(source);
     }
 }
